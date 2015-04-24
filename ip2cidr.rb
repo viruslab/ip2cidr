2c377c0c241ecc29479f37e7460d372a7ee9d972
viruslab <viruslab736@hotmail.com>
2015-04-24 11:27:11 +0800

    version 0.0.1

--- ip2cidr.rb
+++ ip2cidr.rb
@@ -1,159 +1,160 @@
-ip_list = []
-string = "1"
-ip_number = 1
-flag = 0
-
-
-
-
-#converts and IP address to binary
-def ip2binary(ip_string)
-	a = "" 
-	ip_string = ip_string.split(".")
-	ip_string.each {|x| a << "%08b" % x.to_i}
+class String
+  
+  def ip_tobinary
+  a = "" 
+	ip = self.split(".")
+	ip.each { | x | a << "%08b" % x.to_i}
 	return a
-end
-
-def getcidr(ip_first, ip_last)
-
-	a = ip_first
-	b = ip_last
-
-	puts a
-	puts b
-	xor = (a.to_i(2) ^ b.to_i(2)).to_s(2)
-	puts xor
-	cidr = 32 - (xor.length)
-	return cidr
-
-end
-
-#IP2CIDR
-def ip2cidr(ip_1, ip_2)
-
-	bit_value = [1,2,4,8,16,32,64,128]
-	temp_ip = (ip_1 < ip_2) ? ip_1 : ip_2
-	cidr_list = []
-	counter = 0
-	i = 0
-	
-	puts ip_1
-	puts ip_2
-
-	elements = (ip_1.to_i(2) - ip_2.to_i(2)).abs + 1
-
-	temp_elements = elements
-
-	while true
-
-		while elements > bit_value[i]
-			i += 1
-
-		end
-			puts "elements>> #{elements}"
-			
-		if elements == bit_value[i]
-		
-			temp_elements = bit_value[i]
-			elements = elements - temp_elements	
-			temp_ip_last = temp_ip			
-
-				while counter < temp_elements - 1
-				temp_ip_last = "%08b" % (temp_ip_last.to_i(2) + 1)
-				counter += 1
-				end
-				puts temp_ip
-				puts temp_ip_last
-				cidr_list << getcidr(temp_ip, temp_ip_last)
-
-			break
-		end
-			
-		if elements != bit_value[i]
-
-			puts "bit_value>> #{bit_value[i-1]}"
-			temp_elements = bit_value[i-1]
-			puts "temp_elements>> #{temp_elements}"
-			elements = elements - temp_elements	
-			temp_ip_last = temp_ip
-
-			while counter < temp_elements - 1
-				temp_ip_last = "%032b" % (temp_ip_last.to_i(2) + 1)
-				counter += 1
-			end
-				puts temp_ip
-				puts temp_ip_last
-				cidr_list << getcidr(temp_ip, temp_ip_last)
- 
-		else
-			cidr_list << 32
-			break
-
-		end
-		i = 0
-		counter = 0
-	end
-
-	return cidr_list
-
-
-end	
-
-#converts binary to CIDR
-def converttoip(ipaddress)
-	a = ""
-	ipaddress = ipaddress.scan(/......../)
-
-	ipaddress.each {|x| a << x.to_i(2).to_s + "."}
+  end
+  
+  def binary_toip
+  a = ""
+	ip = self.scan(/......../)
+	ip.each {|x| a << x.to_i(2).to_s + "."}
 	return a[0...-1]
-end
+  end
 
-
-def checkifvalid(ip_string)
-	index = 0
+  def valid?
+    index = 0
 	flag = 0
-	count = ip_string.count "."
+	count = self.count "."
+	ip = ""
 
-	if count == 3
-
-		ip_string = ip_string.split(".")
+	if count== 3
+	  ip = self.split(".")
 		
-		while index < 4
-			if ip_string[index].to_i > 255
-				flag += 1
-			end
-			index += 1
+	  while index < 4
+        if ip[index].to_i > 255
+		  flag += 1
 		end
+		  index += 1
+	  end
+      
+      if flag == 0 
+	    return true
+	  end
+	end	   
+     return false
+   end
 
-		if flag == 0 
-			return 1
-		end
-	end
-	
-	return 0
+end
+
+def iprange_tolist(iprange)
+  iplist = []
+  min = iprange[0].to_i(2)
+  max = iprange[1].to_i(2)
+  temp = (min..max).to_a
+  temp.each { | x | iplist << "%032b" % x }
+  return iplist
+end
+
+def IPtoCIDR(iplist)
+  cidrlist = []
+  # powerlist = [1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192]
+  powerlist = Array.new(32) { |n| n = 2**n }
+  cidr_x = 0
+  cidr_y = 0
+
+  ip1 = ""
+  ip2 = ""
+
+  i = 0
+  index = 0
+  counter = 0
+
+  length = iplist.length
+
+  while index < length
+    if iplist[index][-2..-1].to_i == 0 
+      i = 0
+      remaining = length - index
+      counter = index
+      ip1 = iplist[index]
+      counter = index + 2**(ip1.reverse.index('1'))
+
+      while remaining - powerlist[i]  >=  0 #test condition
+        i+= 1
+      end
+      
+      remaining = index + powerlist[i-1]
+      
+      while index < counter - 1 && index < length - 1 && index < remaining - 1
+        index += 1 
+      end
+      ip2 = iplist[index] 
+      cidrlist << getprefix(ip1, ip2)
+      index += 1
+
+    elsif iplist[index][-2..-1].to_i == 1
+    	ip1 = iplist[index]
+        cidrlist << getprefix(ip1,ip1)
+        index += 1
+
+    elsif iplist[index][-2..-1].to_i == 10
+  	  if index + 1 < length &&  iplist[index + 1][-2..-1].to_i == 11
+  	  	ip1 = iplist[index]
+  	  	ip2 = iplist[index + 1]
+  	  	cidrlist << getprefix(ip1, ip2)
+  	    index += 2
+  	  else
+  	  	ip1 = iplist[index]
+  	    cidrlist << getprefix(ip1, ip1)
+  	    index += 1
+  	  end
+
+  	elsif iplist[index][-2..-1].to_i == 11
+  	  ip1 = iplist[index]
+  	  cidrlist << getprefix(ip1, ip1)
+  	  index += 1  		
+    end  
+    counter = 0
+  end  
+  return cidrlist
+end
+
+def getprefix(ip1, ip2)
+  a = ip1
+  b = ip2
+  if ip1 == ip2
+  	prefix = 32
+  else
+  	xor = (a.to_i(2) ^ b.to_i(2)).to_s(2) 
+  	prefix = 32 - xor.length
+  end
+  return ip1.binary_toip << "/" << "#{prefix}"
 end
 
 
+input_count = 0
+user_input = ""
+iprange = []
+iplist = []
+cidrlist = []
 
-puts "please enter IP addresses below. '0' to terminate "
+while input_count < 2 
 
-while ip_list.length < 2
+  case 
+  when input_count == 0  	
+    print "Input minimum IP Address: "
+    user_input = gets.chomp
+  when input_count == 1
+  	print  "Input maximum IP Address: "  
+  	user_input = gets.chomp
+  end
+  STDOUT.flush
+  input_count += 1
 
-	print "IP Address number #{ip_number}: "
-	STDOUT.flush  
-	
-	string = gets.chomp
-	flag = checkifvalid(string)	
-
-		if flag == 0
-			print "The IP Address is either invalid or reserved \n"
-
-		else
-			ip_list << ip2binary(string)
-			ip_number += 1
-		end	
+  if user_input.valid? == false
+    input_count -= 1
+  	p "Please input a valid IP Address"
+  else
+    iprange << user_input.ip_tobinary
+  end
 end
 
+iplist = iprange_tolist(iprange)
+#puts iplist
+cidrlist = IPtoCIDR(iplist)
 
+puts cidrlist
 
-puts "RESULT:  #{ip2cidr(ip_list[0],ip_list[1])}"
