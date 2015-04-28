4882279179e9cbf7f683f5785fdb08347e53ed32
viruslab <viruslab736@otmail.com>
2015-04-28 16:43:51 +0800

    version 0.0.0

---
+++ lib/iptocidr.rb
@@ -0,0 +1,70 @@
+class iptocidr
+  # IPString2Long 
+  # Convert an ip address in string format to integer.
+  def IPString2Long(ipstring)
+    ipstring = ipstring.split(".")
+    longip = Array.new(4)
+    for i in 0..3
+      longip[i] = ipstring[i].to_i
+    end
+    #return longip
+    return (longip[0] << 24) + (longip[1] << 16) + 
+           (longip[2] << 8) + longip[3]
+  end
+
+  # Long2IPString
+  # Convert an ip address in integer format to string.
+  def Long2IPString(longip)
+    ipstring = ""
+    ipstring = (longip >> 24).to_s + "." + 
+               ((longip & 0x00FFFFFF)>> 16).to_s + "." + 
+               ((longip & 0x0000FFFF) >> 8).to_s + "." + 
+               (longip & 0x000000FF).to_s
+    return ipstring
+  end
+
+  # IPRange2CIDR
+  # Return a list of cidr addresses given a range of ip 
+  # addresses.
+  def IPRange2CIDR(startip, endip)
+    cidr2mask = [0x00000000, 0x80000000, 0xC0000000, 
+                 0xE0000000, 0xF0000000, 0xF8000000, 
+                 0xFC000000, 0xFE000000, 0xFF000000, 
+                 0xFF800000, 0xFFC00000, 0xFFE00000, 
+                 0xFFF00000, 0xFFF80000, 0xFFFC0000, 
+                 0xFFFE0000, 0xFFFF0000, 0xFFFF8000, 
+                 0xFFFFC000, 0xFFFFE000, 0xFFFFF000, 
+                 0xFFFFF800, 0xFFFFFC00, 0xFFFFFE00, 
+                 0xFFFFFF00, 0xFFFFFF80, 0xFFFFFFC0, 
+                 0xFFFFFFE0, 0xFFFFFFF0, 0xFFFFFFF8, 
+                 0xFFFFFFFC, 0xFFFFFFFE, 0xFFFFFFFF]
+    startaddr =  IPString2Long(startip)
+    endaddr = IPString2Long (endip)
+    cidrlist = Array.new
+
+    while endaddr >= startaddr
+      maxsize = 32
+      while maxsize > 0
+        mask = cidr2mask[maxsize - 1]
+        maskedbase = startaddr & mask
+
+        if maskedbase != startaddr
+           break
+        end
+
+        maxsize -= 1
+      end
+
+      x = Math.log(endaddr - startaddr + 1) / Math.log(2)
+      maxdiff = 32 - x.floor
+      if maxsize < maxdiff
+        maxsize = maxdiff 
+      end
+
+      cidrlist.push(Long2IPString(startaddr) + "/" + maxsize.to_s)
+      startaddr += 2**(32 - maxsize)
+    end
+
+    return cidrlist
+  end
+end
---
+++ test/test_ip2cidr.rb
@@ -0,0 +1,46 @@
+require 'test/unit'
+require 'ip2cidr'
+
+class CIDRCalculator::IPtoCIDRTest < Test::Unit::TestCase
+	def test_IPString2Long
+		assert_equal 0xc0a80001, CIDRCalculator::IPtoCIDR.IPString2Long("192.168.0.1").to_s(16)
+		assert_equal 0xffffffff, CIDRCalculator::IPtoCIDR.IPString2Long("255.255.255.255").to_s(16)
+		assert_equal 0x00000000, CIDRCalculator::IPtoCIDR.IPString2Long("0.0.0.0").to_s(16)
+		assert_equal 0xfefefefe, CIDRCalculator::IPtoCIDR.IPString2Long("254.254.254.254").to_s(16)
+		assert_equal 0x18ff0701, CIDRCalculator::IPtoCIDR.IPString2Long("24.255.7.1").to_s(16)
+		assert_equal 0xc23f0f60, CIDRCalculator::IPtoCIDR.IPString2Long("194.63.15.96").to_s(16)
+		assert_equal 0x2ef653ca, CIDRCalculator::IPtoCIDR.IPString2Long("46.246.83.202").to_s(16)
+	end
+	
+	def test_Long2IPString
+		assert_equal "192.168.0.1",     CIDRCalculator::IPtoCIDR.Long2IPString(0xc0a80001)
+    assert_equal "255.255.255.255", CIDRCalculator::IPtoCIDR.Long2IPString(0xffffffff)
+    assert_equal "0.0.0.0",         CIDRCalculator::IPtoCIDR.Long2IPString(0x00000000)
+    assert_equal "254.254.254.254", CIDRCalculator::IPtoCIDR.Long2IPString(0xfefefefe)
+    assert_equal "24.255.7.1",      CIDRCalculator::IPtoCIDR.Long2IPString(0x18ff0701)
+    assert_equal "194.63.15.96",    CIDRCalculator::IPtoCIDR.Long2IPString(0xc23f0f60)
+    assert_equal "46.246.83.202",   CIDRCalculator::IPtoCIDR.Long2IPString(0x2ef653ca)
+	end
+
+  def test_IPRange2CIDR
+    test2 = ["192.168.0.0/13", "192.176.0.0/12", "192.192.0.0/10", "193.0.0.0/8", "194.0.0.0/7", 
+             "196.0.0.0/6", "200.0.0.0/5", "208.0.0.0/4", "224.0.0.0/4", "240.0.0.0/5", "248.0.0.0/6", 
+             "252.0.0.0/7", "254.0.0.0/8", "255.0.0.0/9", "255.128.0.0/11", "255.160.0.0/13", "255.168.0.0/29"]
+    
+    test4 = ["0.168.0.1/32", "0.168.0.2/31", "0.168.0.4/30", "0.168.0.8/29", "0.168.0.16/28",
+             "0.168.0.32/27", "0.168.0.64/26", "0.168.0.128/25", "0.168.1.0/24", 
+             "0.168.2.0/23", "0.168.4.0/22", "0.168.8.0/21", "0.168.16.0/20", 
+             "0.168.32.0/19", "0.168.64.0/18", "0.168.128.0/17", "0.169.0.0/16", 
+             "0.170.0.0/15", "0.172.0.0/14", "0.176.0.0/12", "0.192.0.0/10", 
+             "1.0.0.0/8", "2.0.0.0/7", "4.0.0.0/6", "8.0.0.0/5", "16.0.0.0/4", 
+             "32.0.0.0/3", "64.0.0.0/2", "128.0.0.0/2", "192.0.0.0/3", "224.0.0.0/4", 
+             "240.0.0.0/5", "248.0.0.0/6", "252.0.0.0/7", "254.0.0.0/24"]
+
+    assert_equal "192.168.0.0/29",  CIDRCalculator::IPtoCIDR.IPRange2CIDR("192.168.0.0", "192.168.0.7")
+    assert_equal test2,             CIDRCalculator::IPtoCIDR.IPRange2CIDR("192.168.0.0", "255.168.0.7")
+    assert_equal "192.168.0.1/32",  CIDRCalculator::IPtoCIDR.IPRange2CIDR("192.168.0.1", "192.168.0.1")
+    assert_equal test4,             CIDRCalculator::IPtoCIDR.IPRange2CIDR("0.168.0.1",   "254.0.0.255")
+  end
+end
+
+  
