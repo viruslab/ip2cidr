9d90c54f887b13a823476bdf625fba16f6e879aa
viruslab <viruslab736@hotmail.com>
2015-04-29 15:04:06 +0800

    minor patches to version 0.0.1

--- ip2cidr.gemspec
+++ ip2cidr.gemspec
@@ -1,12 +1,13 @@
 Gem::Specification.new do |s|
   s.name                = "ip2cidr"
   s.version             = "0.0.1"
+  s.executables         << 'ip2cidr'
   s.date                = %q{2015-04-28}
   s.summary             = %q{Converts IP Range to CIDR}
   s.description         = %q{Converts a range of IP Adrresses to a list of CIDR Adresses}
   s.authors             = ["Carlo Niño A. Bitoon", "Kirk Gandril B. Madraga", "John Patrick Suelto"]
   s.email               = ["carloninobitoon@yahoo.com","kirkmadraga@outlook.com","jp_suelto@yahoo.com"]
-  s.files               = ["lib/ip2cidr.rb"]
+  s.files               = ["lib/ip2cidr.rb", "lib/ip2cidr/iphandling.rb"]
   s.test_files          = ["test/test_ip2cidr.rb"]
   s.require_paths       = ["lib"]
   s.homepage            = %q{http://rubygems.org/gems/iptocidr}
--- lib/ip2cidr.rb
+++ lib/ip2cidr.rb
@@ -1,66 +1,8 @@
+require "ip2cidr/iphandling"
+
 class IPToCIDR
-  # ipstring_to_long 
-  # Convert an ip address in string format to integer.
-  def self.ipstring_to_long(ipstring)
-    ipstring = ipstring.split(".")
-    longip = Array.new(4)
-    for i in 0..3
-      longip[i] = ipstring[i].to_i
-    end
-    #return longip
-    return (longip[0] << 24) + (longip[1] << 16) + 
-           (longip[2] << 8) + longip[3]
-  end
-
-  # long_to_ip_string
-  # Convert an ip address in integer format to string.
-  def self.long_to_ip_string(longip)
-    ipstring = ""
-    ipstring = (longip >> 24).to_s + "." + 
-               ((longip & 0x00FFFFFF)>> 16).to_s + "." + 
-               ((longip & 0x0000FFFF) >> 8).to_s + "." + 
-               (longip & 0x000000FF).to_s
-    return ipstring
-  end
-
-  # iprange_to_cidr
-  # Return a list of cidr addresses given a range of ip 
-  # addresses.
-  def self.iprange_to_cidr(startip, endip)
-    cidr2mask = [0x00000000, 0x80000000, 0xC0000000, 
-                 0xE0000000, 0xF0000000, 0xF8000000, 
-                 0xFC000000, 0xFE000000, 0xFF000000, 
-                 0xFF800000, 0xFFC00000, 0xFFE00000, 
-                 0xFFF00000, 0xFFF80000, 0xFFFC0000, 
-                 0xFFFE0000, 0xFFFF0000, 0xFFFF8000, 
-                 0xFFFFC000, 0xFFFFE000, 0xFFFFF000, 
-                 0xFFFFF800, 0xFFFFFC00, 0xFFFFFE00, 
-                 0xFFFFFF00, 0xFFFFFF80, 0xFFFFFFC0, 
-                 0xFFFFFFE0, 0xFFFFFFF0, 0xFFFFFFF8, 
-                 0xFFFFFFFC, 0xFFFFFFFE, 0xFFFFFFFF]
-    startaddr =  ipstring_to_long(startip)
-    endaddr = ipstring_to_long (endip)
-    cidrlist = Array.new
-
-    while endaddr >= startaddr
-      maxsize = 32
-      while maxsize > 0
-        mask = cidr2mask[maxsize - 1]
-        maskedbase = startaddr & mask
-
-        break if maskedbase != startaddr
-
-        maxsize -= 1
-      end
-
-      x = Math.log(endaddr - startaddr + 1) / Math.log(2)
-      maxdiff = 32 - x.floor
-      maxsize = maxdiff if maxsize < maxdiff
-
-      cidrlist.push(long_to_ip_string(startaddr) + "/" + maxsize.to_s)
-      startaddr += 2**(32 - maxsize)
-    end
-
-    return cidrlist
+  def self.solveCIDR(startip, endip)
+    cidr = IPHandling.new
+    cidr.iprange_to_cidr(startip,endip)
   end
 end
